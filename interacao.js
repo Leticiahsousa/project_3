0f59f2a95cc99908e6e5b473dc50e4ce2484343d
Leticiahsousa <leticia17.helem@gmail.com>
2025-03-19 07:05:20 -0300

    Adicionado o container de imagem deslizante.
    Precisa de ajustes

--- interacao.js
+++ interacao.js
@@ -1,14 +1,41 @@
 function init(){
-	//alert('it works');
 	var el = document.getElementById('canvas');
 	var myLocation = new google.maps.LatLng(-20.445479673078967, -44.766391548511244);
+	
 	var mapOptions = {
 		center: myLocation,
-		zoom: 4,
+		zoom: 10,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		mapTypeControlOptions: {
 			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
+		},
+		zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+        },
+		styles: [
+            {
+                "featureType": "all",
+                "elementType": "geometry",
+                "stylers": [
+                    { "color": "#242f3e" }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [
+                    { "color": "#38414e" }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    { "color": "#9ca5b3" }
+                ]
+            }
+        ]
 	};
 
 	var myMap = new google.maps.Map(el, mapOptions);
@@ -16,21 +43,35 @@ function init(){
 	var marker = new google.maps.Marker({
 		position: myLocation,
 		map: myMap,
-		animation: google.maps.Animation.BOUNCE,
 		icon: 'images/marcador.png'
 	});
 
-	//var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+    var infoWindow = new google.maps.InfoWindow({
+        content: "<h3>Minha Localização</h3><p>Você está aqui!</p>"
+    });
 
-	var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  	});
-
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
-  	});
-
-
+    marker.addListener("click", function() {
+        infoWindow.open(myMap, marker);
+    });
 }
 
 google.maps.event.addDomListener(window, 'load', init);
+
+document.addEventListener("DOMContentLoaded", function () {
+    let imagens = document.querySelectorAll(".slider img");
+    let indice = 0;
+
+    document.getElementById("prev").onclick = () => {
+        imagens[indice].classList.remove("active");
+        indice = (indice > 0) ? indice - 1 : imagens.length - 1;
+        imagens[indice].classList.add("active");
+    };
+
+    document.getElementById("next").onclick = () => {
+        imagens[indice].classList.remove("active");
+        indice = (indice < imagens.length - 1) ? indice + 1 : 0;
+        imagens[indice].classList.add("active");
+    };
+});
+
+
